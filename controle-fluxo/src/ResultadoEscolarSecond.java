d21650510037aaaa2f8f54eac4ae7f606874320c
rafisgarrido <rafisgarrido@gmail.com>
2023-10-05 08:49:58 -0300

    condicao encadeada

---
+++ controle-fluxo/src/ResultadoEscolarSecond.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolarSecond {
+    public static void main(String[] args) {
+
+        int nota = 8;
+        
+        if(nota >= 7)
+         System.out.println("Aprovado");
+ 
+        else if (nota >= 5 && nota < 7)
+         System.out.println("Prova Recuperação");
+
+         else
+         System.out.println("Reprovado");
+     }
+ }
+
